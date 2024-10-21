e8c276375cf8953cf43d18a00d23310c91f2a02d
Glb <2513554233@qq.com>
2024-10-21 20:04:33 +0800

    补充虚函数

--- OthersTest/Test.cpp
+++ OthersTest/Test.cpp
@@ -2,9 +2,44 @@
 
 using namespace std;
 
+class Animal
+{
+    public:
+        virtual void Sound(){
+            cout<<"Animal makes a sound"<<endl;
+        }
+};
+
+class Dog : public Animal
+{
+    public:
+    void Sound() override{
+        std::cout << "Dog barks" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void Sound() override {
+        std::cout << "Cat meows" << std::endl;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    cout<<"Hello World";
- 
+    Animal* animal;
+    Animal an;
+    Dog dog;
+    Cat cat;
+
+
+    an.Sound();
+
+    animal = &dog;
+    animal->Sound();  // 输出 "Dog barks"
+
+    animal = &cat;
+    animal->Sound();  // 输出 "Cat meows"
+
     return 0;
 }
